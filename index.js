8ac0cd4c17aaaaba788cc9aa89765c6ac7125015
Tarlord <evgeniy.tarl@gmail.com>
2016-06-12 19:05:13 +0300

    Table listing

--- index.js
+++ index.js
@@ -4,6 +4,46 @@ var exec = require('child_process').exec;
 
 var urls = []
 
+var headers = [
+  {label: 'Name', value: 'name', scope: 'channel'},
+  {label: 'Game', value: 'game', scope: 'channel'},
+  {label: 'Viewers', value: 'viewers'},
+  {label: 'Lang', value: 'broadcaster_language', scope: 'channel'},
+  {label: 'Description', value: 'status', scope: 'channel'}]
+
+var screen = blessed.screen({
+  smartCSR: true
+});
+
+var box = blessed.box({
+  parent: screen,
+  width: '100%',
+  height: '100%'
+});
+
+var table = blessed.listtable({
+  parent: screen,
+  width: '100%',
+  height: '100%',
+  label: 'Streams listing',
+  // top: 0,
+  // left: 0,
+  // rows: [],
+  // data: [],
+  selectedFg: 'white',
+  selectedBg: 'grey',
+  keys: true,
+  interactive: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    border: {
+      fg: 'black'
+    }
+  }
+});
+
 var loadStreams = function (params) {
   fetch('https://api.twitch.tv/kraken/streams')
   .then (function(response) {
@@ -16,57 +56,30 @@ var loadStreams = function (params) {
   .then (function(json) {
     if ('streams' in json) {
       var streams = json['streams'];
-      var channelNames = streams.map(function(stream, i, s) {
-        return stream.channel.name;
-      });
+      var data = streams.map(function(stream, index, s) {
+        var streamData = headers.map(function(header) {
+          if (header.scope) {
+            return '' + stream[header.scope][header.value]
+          } else {
+            return '' + stream[header.value]
+          }
+        })
+        return streamData
+      })
+      data.unshift(headers.map(function(header) {return header.label}))
       urls = streams.map(function(stream, i, s) {
         return stream.channel.url;
       });
-      list.clearItems();
-      list.setItems(channelNames);
+      screen.append(table)
+      table.setData(data)
+      table.focus()
       screen.render();
     }
   })
 }
 
-var screen = blessed.screen({
-  smartCSR: true
-});
-
-var box = blessed.box({
-  width: '100%',
-  height: '100%'
-});
-
-screen.append(box);
-
-var list = blessed.list({
-  parent: box,
-  top: 0,
-  left: 0,
-  items: [],
-  keys: true,
-  border: {
-    type: 'line'
-  },
-  style: {
-    border: {
-      fg: '#f0f0f0'
-    },
-    fg: 'white',
-    header: {
-      bg: 'red',
-      fg: 'white'
-    },
-    cell: {
-      bg: 'blue',
-      fg: 'black'
-    }
-  }
-});
-
-list.on('select', function(element, index) {
-  exec('livestreamer ' + urls[index] + ' best', (error, stdout, stderr) => {
+table.on('select', function(element, index) {
+  exec('livestreamer ' + urls[index - 1] + ' best', (error, stdout, stderr) => {
     //
   });
 });
@@ -78,7 +91,4 @@ screen.key('u', function(ch, key) {
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
-
-list.focus();
-screen.render();
 loadStreams();
