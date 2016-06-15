97f24370e27570a127377419b1afd0cf65ae9732
GooNyaa <goonyaa@protonmail.ch>
2016-06-16 02:54:39 +0300

    demo status bar

--- index.js
+++ index.js
@@ -21,18 +21,19 @@ var screen = blessed.screen({
   smartCSR: true
 });
 
-var box = blessed.box({
-  parent: screen,
-  width: '100%',
-  height: '100%'
-});
+// FIXME: do we actually need this?
+// var box = blessed.box({
+//   parent: screen,
+//   width: '100%',
+//   height: '100%'
+// });
 
 // games(screen, fetch)
 
 var table = blessed.listtable({
   parent: screen,
   width: '100%',
-  height: '100%',
+  height: '95%',
   // top: 0,
   // left: 0,
   // rows: [],
@@ -51,23 +52,38 @@ var table = blessed.listtable({
   }
 });
 
-screen.append(table);
+// NOTE: everything works fine without it
+//screen.append(table);
+
+// NOTE: actually need to duplicate everything for loadGames... =(
+var demoStatusBar = blessed.box({
+  parent: screen,
+  width: '100%',
+  height: '5%', // pick some better size
+  bottom: 0,
+});
+
+//screen.append(demoStatusBar);
 
 var loadStreams = function (params) {
   var fetchUrl = 'https://api.twitch.tv/kraken/streams';
   if (params && params.game) {
     fetchUrl += '?game=' + params.game;
   }
+  demoStatusBar.setContent('Listing update in progress');
   fetch(fetchUrl)
   .then (function(response) {
+    demoStatusBar.setContent('Listing update complited');
     if (response.ok) {
       return response.json();
     }
   }, function(error) {
-    console.log(error.message);
+    demoStatusBar.setContent('Listing update errored');
+    //console.log(error.message);
   })
   .then (function(json) {
     if ('streams' in json) {
+      demoStatusBar.setContent('Preparing table for display');
       var streams = json['streams'];
       var data = streams.map(function(stream, index, s) {
         var streamData = headers.map(function(header) {
@@ -87,6 +103,8 @@ var loadStreams = function (params) {
       table.setData(data);
       table.setLabel('Streams listing');
       table.focus();
+      demoStatusBar.setContent('Table has been updated');
+      setTimeout(function() {demoStatusBar.setContent(''); screen.render();}, 500);
       screen.render();
     }
   })
@@ -133,4 +151,5 @@ screen.key('r', function(ch, key) {
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
   return process.exit(0);
 });
+
 loadStreams();
